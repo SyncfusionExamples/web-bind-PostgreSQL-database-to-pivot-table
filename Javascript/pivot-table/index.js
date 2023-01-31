ad3e680c26da7f5bec6fc72950320711b8d2d051
yashvanth <yashvanth.ganesan@syncfusion.com>
2023-01-31 13:46:31 +0530

    documentation(EJ2-68291): To include UG for PostgreSql data binding for EJ2 Pivot Table

--- Javascript/pivot-table/index.js
+++ Javascript/pivot-table/index.js
@@ -1,13 +1,13 @@
 var pivotObj = new ej.pivotview.PivotView({
-	dataSourceSettings: {
+  dataSourceSettings: {
     url: 'https://localhost:7238/Pivot',
     enableSorting: true,
     columns: [{ name: 'openinghours_practice', caption: 'Openinghours Practice' }, { name: 'closinghours_practice', caption: 'Closinghours Practice' }],
     values: [{ name: 'revenue', caption: 'Revenue' }],
     rows: [{ name: 'servicetype', caption: 'Service Type' }, { name: 'servicecategory', caption: 'Service Category' }],
-	},
-	showFieldList: true,
-	width: '100%',
+  },
+  showFieldList: true,
+  width: '100%',
   height: 350,
 });
 pivotObj.appendTo('#PivotView');
